01c6e281a8316b2c34593836b9243d49bb283886
Maxime Ishimwe <mxmishimwe5@gmail.com>
2020-08-02 12:08:31 +0200

    ft: added fade in effect to some divs

--- scripts/messages.js
+++ scripts/messages.js
@@ -37,17 +37,30 @@ document.addEventListener('DOMContentLoaded', () => {
         //save Message
         saveMessage(name, email, phone, message);
 
+        //add fade in effects to sections
+        ['#alert-success', '#submit-btn', '.small-title'].forEach((section, index) => {
+            if (document.querySelector(section).style.animation)
+                document.querySelector(section).style.animation = `sectionFadeIn 0.5s ease-in-out forwards ${index / 7}s`;
+            else 
+                document.querySelector(section).style.animation = `sectionFadeIn 0.5s ease-in-out forwards ${index / 7}s`;
+        });
+
         //alert user that message was successfully sent and hide submit-btn
         document.querySelector('#alert-success').style.display = 'block';
         document.querySelector('#submit-btn').style.display = 'none';
+        document.querySelector('#submit-btn').style.opacity = 0;
+        document.querySelector('.small-title').style.display = 'none';
+        document.querySelector('.small-title').style.opacity = 0;
 
         // hide alert after and show submit-btn after 3s
         setTimeout(() => {
             document.querySelector('#alert-success').style.display = 'none';
             document.querySelector('#submit-btn').style.display = 'block';
+            document.querySelector('.small-title').style.display = 'block';
+
             
             document.querySelector('#contact-form').reset();
-        }, 3000);
+        }, 5000);
     }
 
     //Save (send) messages to firebase collection
