ee1b51d2841d29c44b3d45fe0fd5e2ee1a184d8d
Maxime Ishimwe <mxmishimwe5@gmail.com>
2020-08-17 08:11:30 +0200

    fix: removed firebase.analytics()

--- scripts/init-firebase.js
+++ scripts/init-firebase.js
@@ -1,5 +1,5 @@
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
+// Your web app's Firebase configuration
+var firebaseConfig = {
     apiKey: "AIzaSyBm_cO6kpbZPw-USUP5qNeCeIUUIx8Tb_c",
     authDomain: "maxime-portfolio.firebaseapp.com",
     databaseURL: "https://maxime-portfolio.firebaseio.com",
@@ -8,10 +8,13 @@ firebase.initializeApp({
     messagingSenderId: "277428965287",
     appId: "1:277428965287:web:cd23ba4a4a57c33758f082",
     measurementId: "G-94LP44ZN00"
-});
+};
 
-firebase.analytics();
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
 
+// Initialize Cloud Firestore through Firebase
 var database = firebase.firestore();
 var storage = firebase.storage();
 var storageRef = storage.ref();
